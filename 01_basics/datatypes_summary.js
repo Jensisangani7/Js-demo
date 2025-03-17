de5db6e8e2b550e76805eaad06bad94198caabd6
Jensi_sangani <123874978+Jensisangani7@users.noreply.github.com>
2025-03-17 15:45:34 +0000

    dataTypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,38 @@
+// ....Primitive......
+
+// 7 types : String, Number, Boolean, null, undefined, Symbo;,BigInt
+
+const score = 100           //number
+const scoreValue = 100.3    //number
+
+
+const isLoggedIn = false    //boolean
+const outsideTemp = null    //object
+let userEmail;              //undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigInt = 9785687245829875567982763582983756n; // bigint
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = { 
+    name: "jensi",
+    ahe: 20,
+}
+
+ const myFunction = function(){
+    console.log("Heloo world");
+ }
+
+console.log(typeof(heros));      // object
+console.log(typeof(myFunction)); // function
+
+
